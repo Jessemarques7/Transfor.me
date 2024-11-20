2130ef853170bcca8e5bb978cabda763ae377968
Jesse Marques <jessemateus919@gmail.com>
2024-11-20 19:55:23 -0300

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,17 @@ import { GlobalStyled } from "./globalStyled.jsx";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk publishable key to the .env.local file");
+if (!clerkPublishableKey) {
+  throw new Error(
+    "Missing Clerk publishable key. Ensure it is set in your environment variables."
+  );
 }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={clerkPublishableKey}>
       <App />
       <GlobalStyled />
     </ClerkProvider>
